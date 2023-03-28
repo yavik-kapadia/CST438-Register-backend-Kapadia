a3c861c00ff588d3d50f2036cbbb456d5c34f158
Yavik Kapadia <yavikkapadia@gmail.com>
2023-03-27 21:42:21 -0700

    fixed a bug where grade was not being saved.

--- cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -54,6 +54,7 @@ public class GradebookServiceMQ extends GradebookService {
 			Enrollment tmp = enrollmentRepository.findByEmailAndCourseId(grade.student_email, courseDTOG.course_id);
 			if(tmp != null) {
 				tmp.setCourseGrade(grade.grade);
+				enrollmentRepository.save(tmp);
 				
 			} else {
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not find enrollment record.");
