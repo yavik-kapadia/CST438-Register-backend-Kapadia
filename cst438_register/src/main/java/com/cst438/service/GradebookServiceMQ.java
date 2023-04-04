52cce41ad7f6a1037233e5d918629f8610565dee
Yavik <yavikkapadia@gmail.com>
2023-04-03 21:55:55 -0700

    Added E2E test and fixed errors in service code

--- cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -1,6 +1,5 @@
 package com.cst438.service;
 
-
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -14,54 +13,57 @@ import com.cst438.domain.Enrollment;
 import com.cst438.domain.EnrollmentDTO;
 import com.cst438.domain.EnrollmentRepository;
 
-
 public class GradebookServiceMQ extends GradebookService {
-	
+
 	@Autowired
 	RabbitTemplate rabbitTemplate;
-	
+
 	@Autowired
 	EnrollmentRepository enrollmentRepository;
-	
+
 	@Autowired
 	Queue gradebookQueue;
-	
-	
+
 	public GradebookServiceMQ() {
 		System.out.println("MQ grade book service");
 	}
-	
+
 	// send message to grade book service about new student enrollment in course
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
-		 
-		// TODO 
-		// create EnrollmentDTO and send to gradebookQueue
-		EnrollmentDTO new_enroll =  new EnrollmentDTO(student_email, student_name, course_id);
-		this.rabbitTemplate.convertAndSend(gradebookQueue.getName(), new_enroll);
-		
-		System.out.println("Message send to gradbook service for student "+ student_email +" " + course_id);  
-		
+
+		EnrollmentDTO enrollmentDTO = new EnrollmentDTO(student_email, student_name, course_id);
+
+		rabbitTemplate.convertAndSend(gradebookQueue.getName(), enrollmentDTO);
+
+		System.out.println("Message send to gradbook service for student "
+				+ student_email + " " + course_id);
+
 	}
-	
+
+
 	@RabbitListener(queues = "registration-queue")
 	public void receive(CourseDTOG courseDTOG) {
 		System.out.println("Receive enrollment :" + courseDTOG);
 
-		//TODO 
-		// for each student grade in courseDTOG,  find the student enrollment entity, update the grade and save back to enrollmentRepository.
-		for(CourseDTOG.GradeDTO grade: courseDTOG.grades) {
-			Enrollment tmp = enrollmentRepository.findByEmailAndCourseId(grade.student_email, courseDTOG.course_id);
-			if(tmp != null) {
-				tmp.setCourseGrade(grade.grade);
-				enrollmentRepository.save(tmp);
-
-			} else {
-				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Could not find enrollment record.");
-			}
-			
+		// process the list of student grades
+		for (CourseDTOG.GradeDTO g : courseDTOG.grades) {
+			Enrollment e = enrollmentRepository.findByEmailAndCourseId(
+					g.student_email,
+					courseDTOG.course_id);
+			e.setCourseGrade(g.grade);
+			enrollmentRepository.save(e);
+			System.out.println("final grade update " + g.student_email
+					+ " " + courseDTOG.course_id + " " + g.grade);
 		}
-		
 	}
 
+
 }
+
+
+
+
+
+
+
